9c9dc3db2caca7d29f5a2469ed0cd2673a06ebf1
jonathanbataire <jonathanbataire@gmail.com>
2024-09-29 09:41:11 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
-import logo from './assets/logo.svg';
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from 'react-router-dom';
+
+//import logo from './assets/logo.svg';
 //import { Counter } from '@components/Counter';
 import { Login } from '@components/Login';
 import '@styles/App.scss';
+import { Home } from '@components/Home';
+
+const rootElement = <div id='route-outlet'><Outlet/></div>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Login />
-      </header>
-    </div>
-  );
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: rootElement,
+      errorElement: <div>error</div>,
+      children: [
+        {
+          index: true,
+          element: <Login />,
+        },
+        {
+          path: 'login',
+          element: <Navigate to='/'/>
+        },
+        {
+          path: 'home',
+          element: <Home/> //aka layout protect this
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <div>Not found</div>
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
